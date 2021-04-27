5944f1daf70820a12e7aa380bbebf37121365617
AnastasiaVerich <a.verich.m@gmail.com>
2021-04-27 14:02:31 +0300

    add reduce

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,13 @@ import {
     Container,
     Grid,
     IconButton,
-    Menu,
     MenuItem,
     Paper,
     Toolbar,
     Typography
 } from "@material-ui/core";
 
-type TodolistType = {
+export type TodolistType = {
     id: string
     title: string
     filter: FilterType
@@ -32,13 +31,13 @@ function App() {
     let TDid2 = v1()
     let TDid3 = v1()
     //массив с айдишниками тудуЛистов, их названиями и активными фильтрами(по умолчанию желательно фильтр all)
-    let [arrayAllTodolists, setArrayAllTodolists] = useState<Array<TodolistType>>([
+    let [todolists, SetTodoLists] = useState<Array<TodolistType>>([
         {id: TDid1, title: "What to learn", filter: "active"},
         {id: TDid2, title: "Second Tasks", filter: "complited"},
         {id: TDid3, title: "3 Tasks", filter: "all"}
     ])
     // массив задач для тудулистов
-    let [tasksForAllTodolists, setTasksForAllTodolists] = useState<tasksType>({
+    let [tasksArray, setTasks] = useState<tasksType>({
         [TDid1]: [
             {id: v1(), title: "HTML", isDone: true},
             {id: v1(), title: "JS", isDone: true},
@@ -54,42 +53,42 @@ function App() {
     })
 
 
-    //удалить один тудуЛист. По факту мы меняем исхоные два осноных массива(тасок и ТЛ) и заново по ним реакт отрисовывает приложение
+//удалить один тудуЛист. По факту мы меняем исхоные два осноных массива(тасок и ТЛ) и заново по ним реакт отрисовывает приложение
     let remuveTodoList = (todolistid: string) => {
         debugger
         //фильтруем массив тудуЛистов. В отфильтрованный массив попадают не выбранные таски
-        let filterArrayTLS_withoutSelectedTL = arrayAllTodolists.filter(x => x.id !== todolistid);
-        setArrayAllTodolists(filterArrayTLS_withoutSelectedTL);
-        delete tasksForAllTodolists[todolistid];//удаляем в обьекте тасок такску под выбранным айди
-        setTasksForAllTodolists({...tasksForAllTodolists})
+        let filter = todolists.filter(x => x.id !== todolistid);
+        SetTodoLists(filter);
+        delete tasksArray[todolistid];//удаляем в обьекте тасок такску под выбранным айди
+        setTasks({...tasksArray})
     }
 
 
 //delete choose task
     function deleteTask(id: string, IdSelectedTL: string) {
-        let tasks = tasksForAllTodolists[IdSelectedTL]
+        let tasks = tasksArray[IdSelectedTL]
 
         let filterTask = tasks.filter((t) => t.id !== id)
-        tasksForAllTodolists[IdSelectedTL] = filterTask
-        setTasksForAllTodolists({...tasksForAllTodolists})
+        tasksArray[IdSelectedTL] = filterTask
+        setTasks({...tasksArray})
     }
 
 // add new task
     function addTask(title: string, todolistId: string) {
         let newTask = {id: v1(), title: title, isDone: true}
-        let tasks = tasksForAllTodolists[todolistId]
+        let tasks = tasksArray[todolistId]
         let newTasks = [newTask, ...tasks];
-        tasksForAllTodolists[todolistId] = newTasks
-        setTasksForAllTodolists({...tasksForAllTodolists})
+        tasksArray[todolistId] = newTasks
+        setTasks({...tasksArray})
     }
 
 // change isDone task
     function changeStatus(taskID: string, isDone: boolean, todolistId: string) {
-        let tasks = tasksForAllTodolists[todolistId]
+        let tasks = tasksArray[todolistId]
         let task = tasks.find(t => t.id === taskID);
         if (task) {
             task.isDone = isDone
-            setTasksForAllTodolists({...tasksForAllTodolists})
+            setTasks({...tasksArray})
         }
     }
 
@@ -97,10 +96,10 @@ function App() {
 
 // меняет  значение фильтра
     function changefilters(value: FilterType, TDid: string) {
-        let toDo = arrayAllTodolists.find(x => x.id === TDid);
+        let toDo = todolists.find(x => x.id === TDid);
         if (toDo) {
             toDo.filter = value
-            setArrayAllTodolists([...arrayAllTodolists])
+            SetTodoLists([...todolists])
         }
     }
 
@@ -111,21 +110,21 @@ function App() {
             title: title,
             filter: "all"
         }
-        setArrayAllTodolists([...arrayAllTodolists, newTodoList])
-        setTasksForAllTodolists({...tasksForAllTodolists, [newTodoList.id]: []})
+        SetTodoLists([...todolists, newTodoList])
+        setTasks({...tasksArray, [newTodoList.id]: []})
     }
 
     // change title in task
     function changeTitleTask(taskID: string, title: string, todolistId: string) {
         //достаем нужный массив тасок по айди тудлиста
-        let tasks = tasksForAllTodolists[todolistId]
+        let tasks = tasksArray[todolistId]
         //находим в этом массиве нужную таску
         let task = tasks.find(t => t.id === taskID);
         // если таска существует, меняем ее
         if (task) {
             task.title = title
             // создаем и сетамем копию обьекта, что бы реакт отрисовал
-            setTasksForAllTodolists({...tasksForAllTodolists})
+            setTasks({...tasksArray})
         }
     }
 
@@ -134,12 +133,12 @@ function App() {
     function changeTitleTodolist(title: string, todolistId: string) {
         //достаем нужный массив тасок по айди тудлиста
         //находим в этом массиве нужную таску
-        let td = arrayAllTodolists.find(t => t.id === todolistId);
+        let td = todolists.find(t => t.id === todolistId);
         // если таска существует, меняем ее
         if (td) {
             td.title = title
             // создаем и сетамем копию обьекта, что бы реакт отрисовал
-            setArrayAllTodolists([...arrayAllTodolists])
+            SetTodoLists([...todolists])
         }
     }
 
@@ -163,10 +162,10 @@ function App() {
                     <AddInputForm addItem={addTodoList}/>
                 </Grid>
                 <Grid container spacing={3}>
-                    {arrayAllTodolists.map
+                    {todolists.map
                     ((x) => {
                         //помещаем в переменную все задачи для тудулиста(по одинаковым айди)
-                        let arrayTasksForONEtodolist = tasksForAllTodolists[x.id];
+                        let arrayTasksForONEtodolist = tasksArray[x.id];
                         if (x.filter === "complited") {
                             arrayTasksForONEtodolist = arrayTasksForONEtodolist.filter(t => t.isDone === true)
                         }
---
+++ src/state/todolistsReducer.ts
@@ -0,0 +1,75 @@
+import {FilterType, TodolistType} from "../App";
+import {v1} from "uuid";
+
+
+export type REMOVETODOLIST={
+    type: "REMOVE-TODOLIST"
+    id: string
+}
+export type ADDTODOLIST={
+    type: "ADD-TODOLIST"
+    title: string
+}
+export type CHANGETODOLISTTITLE={
+    type: "CHANGE-TODOLIST-TITLE"
+    id: string
+    title:string
+}
+export type CHANGETODOLISTFILTER={
+    type: "CHANGE-TODOLIST-FILTER"
+    id: string
+    filter: FilterType
+}
+
+type common=REMOVETODOLIST | ADDTODOLIST | CHANGETODOLISTTITLE | CHANGETODOLISTFILTER
+
+export const todolistsReducer = (state: Array<TodolistType>, action: common): Array<TodolistType> => {
+    switch (action.type) {
+        case "REMOVE-TODOLIST":{
+           return state.filter(x => x.id !== action.id)
+        }
+        case "ADD-TODOLIST":{
+            let newTodoList: TodolistType = {
+                id: v1(),
+                title: action.title,
+                filter: "all"
+            }
+            return ([
+                ...state,
+                newTodoList
+            ])
+        }
+        case "CHANGE-TODOLIST-TITLE":{
+            let toDo = state.find(x => x.id === action.id);
+            if (toDo) {
+                toDo.title = action.title
+            }
+            return [...state]
+        }
+        case "CHANGE-TODOLIST-FILTER":{
+            let toDo = state.find(x => x.id === action.id);
+            if (toDo) {
+                toDo.filter = action.filter
+            }
+            return [...state]
+        }
+        default:
+            throw new Error("I don't understand this type")
+    }
+}
+
+export const RemoveTodolistAC = (todolistId: string): REMOVETODOLIST => {
+    return { type: 'REMOVE-TODOLIST', id: todolistId}
+}
+
+export const AddTodolistAC = (title: string): ADDTODOLIST => {
+    return { type: 'ADD-TODOLIST', title: title}
+}
+
+export const CHANGETODOLISTTITLEAC = (title: string, id:string): CHANGETODOLISTTITLE => {
+    return { type: 'CHANGE-TODOLIST-TITLE', id:id, title:title}
+}
+
+export const CHANGETODOLISTFILTERAC = (filter: FilterType, id:string): CHANGETODOLISTFILTER => {
+    return { type: 'CHANGE-TODOLIST-FILTER', id:id, filter:filter}
+}
